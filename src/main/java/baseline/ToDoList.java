5e948fe0a12c318b5cc7a41edf0f5efc0053cf72
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-23 20:30:05 -0500

    Added pseudocode for ToDoList object methods

--- src/main/java/baseline/ToDoList.java
+++ src/main/java/baseline/ToDoList.java
@@ -5,41 +5,71 @@
 
 package baseline;
 
-import java.util.LinkedList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ToDoList {
+    String path;
     String title;
-    LinkedList<Task> tasks;
+    ObservableList<Task> tasks = FXCollections.observableArrayList();
 
     /**
      * Adds a Task to this ToDoList
      * @param description The description of the Task to add.
-     * @return The newly created Task.
+     *
+     * Create a new Task using the provided description
+     * Add the task to this ToDoList
+     * Save the ToDoList
      */
-    public Task addTask(String description) {
-
-        return new Task();
+    public void addTask(String description) {
+        return;
     }
 
     /**
-     * Deletes this ToDoList.
+     * Removes the provided Task from this ToDoList
+     * @param task The Task to remove from this ToDoList
      */
-    public void deleteTask() {
-
+    public void removeTask(Task task) {
+        return;
     }
 
     /**
-     * Renames this ToDoList.
-     * @param newName The new name to set for this ToDoList.
+     * Deletes all Tasks from this ToDoList
+     *
+     * ---
+     * Remove all Tasks from this ToDoList's list of Tasks
+     * Save the ToDoList
+     * ---
      */
-    public void rename(String newName) {
-
+    public void clearAllTasks() {
+        this.tasks.clear();
+        this.save();
     }
 
     /**
-     * Deletes this ToDoList
+     * Initializes the ToDoList's file with json containing its basic properties.
+     *
+     * ---
+     * Create a new FileWriter
+     * Use Gson to convert this ToDoList to json format
+     * Use the FileWriter to write the json string to the file
+     * Flush and close the file
+     * ---
      */
-    public void delete() {
+    public void save() {
 
+        return;
+    }
+
+    /**
+     * Gets the name of this ToDoList.
+     * @return The name of this ToDoList.
+     */
+    public String toString() {
+        return this.title;
     }
 }
