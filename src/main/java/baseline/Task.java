884ca1946c3e37603834f221c299aab3c356a165
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-11-23 20:29:30 -0500

    Added pseudocode for Task object methods

--- src/main/java/baseline/Task.java
+++ src/main/java/baseline/Task.java
@@ -8,20 +8,55 @@ package baseline;
 import java.util.Date;
 
 public class Task {
-    String description;
+    private String description;
 
     /**
      * Formatted as YYYY-MM-DD
      */
     Date dueDate;
-    Date dateCreated;
-    Date dateCompleted;
-    boolean isComplete;
+    private boolean isComplete;
 
     /**
-     * Deletes the Task.
+     * Sets whether this Task is complete
+     * @param complete The new completion status of this Task
      */
-    public void delete() {
+    public void setCompletion(boolean complete) {
+        this.isComplete = complete;
+    }
 
+    /**
+     * Gets whether this Task is complete
+     * @return Whether this task is complete
+     */
+    public boolean isComplete() {
+        return this.isComplete;
+    }
+
+    /**
+     * Sets the description of this Task
+     * @param description The description to set for this Task
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets this Task's description.
+     * @return This Task's description.
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Gets a String with the Task's completion status and description
+     * @return A String with the Task's completion status and description
+     */
+    public String toString() {
+        if (this.isComplete()) {
+            return String.format("(Complete) --- %s", this.description);
+        } else {
+            return String.format("(Incomplete) --- %s", this.description);
+        }
     }
 }
